159b664034c888a53ea995d2026ec8e141b53a3b
FaishalPutra <emfaishalpe@gmail.com>
2025-02-14 19:01:22 +0700

    initial commit

---
+++ Frontend/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".hero-text, .hero-image");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    }, { threshold: 0.2 });
+
+    elements.forEach(el => observer.observe(el));
+});
